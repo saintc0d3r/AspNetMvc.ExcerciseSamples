18e50bd86f0a49216960dcb767b3ed0db69e692f
Wendy Sanarwanto <saintc0d3r@gmail.com>
2014-12-20 23:19:39 +0800

    Initial commit: - Added PartyInvites sample ("Pro ASP NET MVC 4" by Apress - Chapter 2)

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Handles RsvpForm's postback event.
+        /// </summary>
+        /// <param name="guestResponse"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult RsvpForm(GuestResponse guestResponse)
+        {
+            // Check model's validity
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    // Sending invitation mail
+                    sendingRsvpNotificationMail(guestResponse);
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, "Sorry - we couldn't send the email to confirm your RSVP.");
+                    return View();
+                }
+                return View("Thanks", guestResponse);
+            }
+
+            // Model is invalid
+            return View();
+        }
+
+        private void sendingRsvpNotificationMail(GuestResponse model)
+        {
+            WebMail.SmtpServer = "localhost";
+            WebMail.SmtpPort = 25;
+            WebMail.From = "do-not-reply@party-invites.com";
+            WebMail.Send(@"saintc0d3r@gmail.com", "RSVP notification", model.Name + " is " + (model.WillAttend.Value ? "" : "not ") + "attending");
+        }
+    }
+}
---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage="Please enter your name.")]
+        public string Name { set; get; }
+
+        [Required(ErrorMessage="Please enter your email address.")]
+        [EmailAddress]
+        public string Email { set; get; }
+
+        [Required(ErrorMessage="Please enter your phone number.")]
+        [PhoneAttribute]
+        public string Phone { set; get; }
+
+        [Required(ErrorMessage="Please specify whether you will attend.")]
+        public bool? WillAttend { set; get; }
+    }
+}
